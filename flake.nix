2b1ea27c7f13b7783771bd4d753e7a7bb1f10215
killioiden <derek.verduijn@proton.me>
2026-07-02 01:33:52 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          m4
+          rustc
+          rustup
+        ];
+      };
+    };
+}
