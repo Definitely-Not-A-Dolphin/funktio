70e8c527c12670cc2860a08c453c6f8edc75e904
killioiden <derek.verduijn@proton.me>
2026-07-10 22:12:50 +0200

    V2 is completely done :DD

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::{DynamicImage, GenericImageView, ImageReader, RgbaImage};
+use image::{DynamicImage, GenericImage, GenericImageView, ImageReader, RgbaImage};
 use num_complex::Complex32;
 
 #[derive(Clone, Copy, Debug)]
@@ -26,36 +26,30 @@ impl Coordinate {
 }
 
 trait ToImage {
-    fn to_image(self, size: u32) -> Coordinate;
+    fn to_image(self, size: u32) -> Option<Coordinate>;
 }
 
 impl ToImage for Complex32 {
-    fn to_image(self, size: u32) -> Coordinate {
-        let size = size as f32;
-        Coordinate::new(
-            ((size - 1.) * (self.re + 1.) / 2.).round() as u32,
-            ((size - 1.) * (1. - self.im) / 2.).round() as u32,
-        )
+    fn to_image(self, size: u32) -> Option<Coordinate> {
+        let size = size as f32 - 1.;
+
+        if -1. <= self.re && self.re <= 1. && -1. <= self.im && self.im <= 1. {
+            Some(Coordinate::new(
+                (size * (self.re + 1.) / 2.).round() as u32,
+                (size * (1. - self.im) / 2.).round() as u32,
+            ))
+        } else {
+            None
+        }
     }
 }
 
 fn inverse_transformation(z: Complex32) -> Complex32 {
-    z.exp()
+    z.atan()
 }
 
 fn main() {
-    // let coordinate_input = Coordinate::new(0, 200);
-    // let number_input = coordinate_input.to_math(256);
-    // let number_output = transformation_function(number_input);
-    // let coordinate_output = number_output.to_image(256);
-    // dbg!(
-    //     coordinate_input,
-    //     number_input,
-    //     number_output,
-    //     coordinate_output
-    // );
-
-    let img_path = "static/big_malko.jpg";
+    let img_path = "static/myimage.png";
     let img = match ImageReader::open(img_path) {
         Ok(img_reader) => match img_reader.decode() {
             Ok(dynamic_image) => dynamic_image,
@@ -65,9 +59,9 @@ fn main() {
     };
     let (size_x, size_y) = img.dimensions();
     let square_size = if size_x < size_y { size_y } else { size_x };
-    let mut square_img = RgbaImage::new(square_size, square_size);
-
+    let mut square_img: DynamicImage = RgbaImage::new(square_size, square_size).into();
     let extra_height = ((size_y as f32 - size_x as f32).abs() / 2.) as u32;
+
     if size_x < size_y {
         for (x, y, color) in img.pixels() {
             // We need to place pixels to the left and right
@@ -80,17 +74,19 @@ fn main() {
         }
     };
 
-    let square_img: DynamicImage = square_img.into();
-    let mut transformed_img = RgbaImage::new(square_size, square_size);
+    let mut transformed_img: DynamicImage = RgbaImage::new(square_size, square_size).into();
 
-    for (x, y, color) in square_img.pixels() {
-        let input_coordinate = Coordinate::new(x, y);
-        let input_number = input_coordinate.to_math(square_size);
-        let output_number = transformation_function(input_number);
-        //dbg!(input_number, output_number);
-        let ouput_coordinate = output_number.to_image(square_size);
-        if !ouput_coordinate.is_out_of_bounds(square_size) {
-            (&mut transformed_img).put_pixel(ouput_coordinate.x, ouput_coordinate.y, color);
+    for (x, y, _) in transformed_img.clone().pixels() {
+        let output_coordinate = Coordinate::new(x, y);
+        let output_number = output_coordinate.to_math(square_size);
+        let input_number = inverse_transformation(output_number);
+        let input_coordinate = match input_number.to_image(square_size) {
+            Some(input_coordinate) => input_coordinate,
+            None => continue,
+        };
+        if !input_coordinate.is_out_of_bounds(square_size) {
+            let input_color = (&square_img).get_pixel(input_coordinate.x, input_coordinate.y);
+            (&mut transformed_img).put_pixel(x, y, input_color);
         }
     }
 
